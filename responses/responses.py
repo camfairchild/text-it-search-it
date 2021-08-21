8a3c2624ddcecd94768cd4c564231b463a164c5e
Vrim <CarbonAmericiumFluorine@gmail.com>
2021-08-21 12:38:35 -0400

    changed to startswith

--- responses/responses.py
+++ responses/responses.py
@@ -12,21 +12,21 @@ from .search import search_message
 def get_response(body: str) -> str:
     if (body.startswith('!')):
         body = body[1:]
-        if (body == 'help'):
+        if (body.startswith('help')):
             return help_message()
-        elif (body == 'weather'):
+        elif (body.startswith('weather')):
             return weather_message(body[7:])
-        elif (body == 'news'):
+        elif (body.startswith('news')):
             return news_message(body[4:])
-        elif (body == 'translate'):
+        elif (body.startswith('translate')):
             return translate_message(body[9:])
-        elif (body == 'joke'):
+        elif (body.startswith('joke')):
             return joke_message()
-        elif (body == 'time'):
-            return time_message()
-        elif (body == 'date'):
-            return date_message()
-        elif (body == 'directions'):
+        elif (body.startswith('time')):
+            return time_message(body[4:])
+        elif (body.startswith('date')):
+            return date_message(body[4:])
+        elif (body.startswith('directions')):
             return directions_message(body[10:])
         else:
             return error_message()
