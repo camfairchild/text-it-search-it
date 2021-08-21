ca000058ff76daac3f8558e4c387e9c898c1c30b
Vrim <CarbonAmericiumFluorine@gmail.com>
2021-08-21 17:45:42 -0400

    fixed time

--- responses/time.py
+++ responses/time.py
@@ -3,6 +3,6 @@ from .helpers import google
 
 def time_message(time_str: str) -> str:
     """Returns a message of the current time"""
-    lat, long = google.get_location(time_str)
-    time = google.get_time_by_lat_long(lat, long)
+    coords = google.get_location(time_str)
+    time = google.get_time_by_lat_long(coords)
     return f"It is currently {time} in {time_str}"
