209a4882d555ece042f72a2abaecf9d93796fc56
Vrim <CarbonAmericiumFluorine@gmail.com>
2021-08-22 04:31:16 -0400

    remove snippet

--- responses/search.py
+++ responses/search.py
@@ -23,10 +23,9 @@ def search_message(conn, phone_number: str, query_str: str) -> str:
     for i, search_item in enumerate(search_items, start=1):
 
         title = search_item.get("title")
-        snippet = search_item.get("snippet")
         link = search_item.get("link")
 
-        resultStr += f"[{i}] Title: {title}\nDescription: {snippet}\n\n"
+        resultStr += f"[{i}] Title: {title}\n"
 
         options.append(link)
     set_user(conn, phone_number, options, "search")
