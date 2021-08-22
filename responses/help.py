8ca81286b7cc6f4058a84ca71caef132f230073b
Vrim <CarbonAmericiumFluorine@gmail.com>
2021-08-21 21:09:22 -0400

    changed help

--- responses/help.py
+++ responses/help.py
@@ -1,3 +1,11 @@
 def help_message() -> str:
     """Return a list of commands"""
-    return "List of commands:\n" + "!date <location> - Returns the date in the city proceeding the command\n" + "!directions <starting point> <destination> - Returns a list of directions from a starting location to a destination\n" + "!joke - Returns a joke\n" + "!news - Returns a list of news articles relevant to a location\n" + "!time <location> - Returns the time in the city proceeding the command\n" + "!translate <text> - Translates the text proceeding the command\n" + "!weather <location> - Returns the weather in the city proceeding the command\n" + "<text> - Returns a list of results from a Google search\n"
+    return "List of commands:\n" +\
+        "/date <location> - Returns the date in the location proceeding the command\n" +\
+        "/directions <starting point> <destination> - Returns a list of directions from a starting location to a destination\n" +\
+        "/joke - Returns a random joke\n" +\
+        "/news - Returns a list of world news articles from the NY Times\n" +\
+        "/time <location> - Returns the time in the location proceeding the command\n" +\
+        "/translate <text> -> <language> - Translates the text proceeding the command\n" +\
+        "/weather <location> - Returns the weather in the city proceeding the command\n" +\
+        "<query> - Returns a list of results from a Google search\n"
